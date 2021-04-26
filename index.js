6e994cf91b6c3cc269d68e74e0cd46eb31838337
wamu_M <43519854+wamuM@users.noreply.github.com>
2021-04-26 12:43:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,49 @@ function isOnline(server){
 function fetchData(server){
 
 }
+/**
+* A cache is an object that is going to cache the request data
+* @param {String} ip The host's ip or domain 
+* @param {Number} ttlInMs The Time-To-Live in MS
+* @param {Boolean} [loadBackup] if a back up needs to be loaded when having an error at getting data (default is true)
+* @param {Boolean} [restartTTLWhenError] if the ttl countdown needs to be restarted when having an error at getting data
+*/
 class MyCache{
-    constructor(ip,ttlInMs,loadBackup){
+    constructor(ip,ttlInMs,loadBackup,restartTTLWhenError){
         this.ip = ip;
         this.ttl = ttlInMs
-        this.loadBackup = loadBackup || true; 
+        this.loadBackup = loadBackup || true;
+        this.restart  = restartTTLWhenError || true;
         this.lastTime = Date.now()
         try{
-        this.data = await fetchData(this.ip)
+            this.data = await fetchData(this.ip)
         }catch(e){
-        this.data = e;
+            this.data = e;
         }
     }
+    /**
+    * gets the data from the cache or fetchs it from the server if the ttl countdown end 
+    * @returns {Promise} 
+    * @resolves 
+    * @rejects 
+    */
     getData(){
        if(Date.now()>=(this.lastTime+this.ttl)){
             return new Promise((resolve,reject)=>{
-                fetchData(this.ip)
+                fetchData(this.ip).then((data)=>{
+                    resolve(data)
+                    this.data = data;
+                }).catch((err)=>{
+                    if(!this.loadBackup)this.data = err;
+                    if(this.restart)this.lastTime = Date.now();
+                    reject(err)
+                });
             })
        }
        else return Promise.resolve(this.data)
+    }
+    forceFetch(ip=this.ip){
+            
     }
 }
 function request(ip){
